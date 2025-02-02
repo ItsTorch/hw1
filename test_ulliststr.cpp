0405df228bd7e36646110276e6b00e7a0586659e
Christopher Mai <cnmai@usc.edu>
2025-02-02 01:47:05 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,43 @@
 
 int main(int argc, char* argv[])
 {
+  std::cout << "Checking" << std::endl;
+
+  ULListStr dat;
+  std::cout << "test constructor" << std::endl;
+
+  dat.push_back("7");
+  std::cout << "test push back" << std::endl;
+  dat.push_front("8");
+  std::cout << "test push front" << std::endl;
+
+  dat.push_back("9");
+  std::cout << "test push back" << std::endl;
+
+  std::cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << std::endl;
+  std::cout << "test get" << std::endl;
+  // should print 8 7 9
+
+  std::cout << "test size" << std::endl;
+  std::cout << dat.size() << std::endl;
+  //should print 3 for 3 strings stored
+
+  dat.pop_back();
+  std::cout << "test pop back, checking size" << std::endl;
+  std::cout << dat.size() << std::endl;
+  std::cout << "should print 8 7" << std::endl;
+  std::cout << dat.get(0) << " " << dat.get(1) << std::endl;
+
+  dat.pop_front();
+  std::cout << "test pop front, checking size" << std::endl;
+  std::cout << dat.size() << std::endl;
+
+  std::cout << "should print 7" << std::endl;
+  std::cout << dat.get(0) << std::endl;
+
+  dat.push_front("hello");
+  dat.push_back("my friend");
+  std::cout << "should print hello my friend" << std::endl;
+  std::cout << dat.front() << " " << dat.back() << std::endl;
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -26,6 +26,144 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 
+void ULListStr::push_back(const std::string& val) {
+  // if current tail is full, create new item, or if
+  // no tail exists, create new item
+  if ((tail_ == NULL) || (tail_->last == ARRSIZE)) {
+    Item* newItem = new Item();
+    newItem->first = 0;
+    newItem->last = 0;
+    newItem->prev = tail_;
+    newItem->next = NULL;
+
+    if (tail_) {
+      tail_->next = newItem;
+    }
+    tail_ = newItem;
+    
+    if (!head_) {
+    head_ = tail_;
+    }
+  }
+  // insert value at the end of tail
+  tail_->val[tail_->last++] = val;
+  size_++;
+}
+
+void ULListStr::push_front(const std::string& val) {
+  // if no room before the first item, or no head, create
+  //new item
+  if((head_ == NULL) || (head_->first == 0)) {
+    Item* newItem = new Item();
+    newItem->first = ARRSIZE;
+    newItem->last = ARRSIZE;
+    newItem->next = head_;
+    newItem->prev = NULL;
+
+    if (head_) {
+      head_->prev = newItem;
+    }
+    head_ = newItem;
+
+    if(!tail_) {
+      tail_ = head_;
+    }
+  }
+  // insert value at beginning of head
+  head_->val[--head_->first] = val;
+  size_++;
+}
+
+void ULListStr::pop_back() {
+  // does nothing to empty string
+  if (empty()) {
+    return;
+  }
+
+  // removes last element, decrease size
+  tail_->last--;
+  size_--;
+
+  // if tail is empty, remove item there
+  if (tail_->first == tail_->last) {
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if (tail_) {
+      tail_->next = NULL;
+    }
+    else {
+      head_ = NULL;
+    }
+    delete temp;
+  }
+}
+
+void ULListStr::pop_front() {
+  // does nothing to empty string
+  if (empty()) {
+    return;
+  }
+  
+  // removes first element and decrease size
+  head_->first++;
+  size_--;
+
+  // if head is empty, remove it
+  if (head_->first == head_->last) {
+    Item* temp = head_;
+    head_ = head_->next;
+    if (head_) {
+      head_->prev = NULL;
+    }
+    else {
+      tail_ = NULL;
+    }
+    delete temp;
+  }
+}
+
+std::string const& ULListStr::back() const {
+  // checks if list is empty
+  if (empty()) {
+    throw std::out_of_range("List is empty");
+  }
+
+  // returns reference to last element
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const& ULListStr::front() const {
+  // checks if list is empty
+  if (empty()) {
+    throw std::out_of_range("List is empty");
+  }
+
+  // returns reference to first element
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  // if searched location is outside of string, return nothing
+  if (loc >= size_) {
+    return NULL;
+  }
+
+  // create a pointer at the head
+  Item* current = head_;
+
+  // goes through the list
+  while (current) {
+    size_t counter = current->last - current->first;
+    if (loc < counter) {
+      return &current->val[current->first + loc];
+    }
+    loc -= counter;
+    current = current->next;
+  }
+  
+  return NULL;
+}
+
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
